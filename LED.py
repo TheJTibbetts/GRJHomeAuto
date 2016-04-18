d1771b8c998c28830df19bffe5fdbe2029229845
Jake <jtibbetts@salisbury6c.ac.uk>
2016-04-18 12:45:14 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,3 +1,16 @@
-import RPi.GPIO as gpio
+from Board import board
 
 class led:
+  def __init__(self, board, pin):
+    self.board = board
+    self.pin = pin
+    self.setup_led()
+  
+  def setup_led(self):
+    self.board.GPIO.setup(self.pin, self.board.GPIO.OUT)
+  
+  def ledOn(self):
+    self.board.GPIO.output(self.pin, self.board.GPIO.HIGH)
+  
+  def ledOff(self):
+    self.board.GPIO.output(self.pin, self.board.GPIO.LOW)
