18ec80ece14537de2ba6e8dfd3dea23f0a0a9570
rjamesS6C <rjames@salisbury6c.ac.uk>
2016-04-19 09:22:36 +0100

    Create Buzzer.py

---
+++ Buzzer.py
@@ -0,0 +1,16 @@
+from Board import board
+
+class buzzer:
+  def __init__(self, board, pin):
+    self.board = board
+    self.pin = pin
+    self.setup_buzzer()
+  
+  def setup_buzzer(self):
+    self.board.GPIO.setup(self.pin, self.board.GPIO.OUT)
+  
+  def buzzerOn(self):
+    self.board.GPIO.output(self.pin, self.board.GPIO.HIGH)
+    
+  def buzzerOff(self):
+    self.board.GPIO.output(self.pin, self.board.GPIO.LOW)
