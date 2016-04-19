27842a515efe36adc268c18a49dfe67c97814db5
rjamesS6C <rjames@salisbury6c.ac.uk>
2016-04-19 09:33:22 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from BOARD import BOARD_CL
 from LCD import LCD1602_CL
-from Button import button
+from BUTTON import BUTTON_CL
 from LED import LED_CL
 from Temp import temp
 from Motion import motion
