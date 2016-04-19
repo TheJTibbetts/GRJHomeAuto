dc97e894cae3e4cf04c1b4d1734706bc0bcc90a8
rjamesS6C <rjames@salisbury6c.ac.uk>
2016-04-19 09:48:51 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from BOARD import BOARD_CL
 from LCD import LCD1602_CL
 from BUTTON import BUTTON_CL
 from LED import LED_CL
+from CLOCK import CLOCK_CL
+from BUZZER import BUZZER_CL
 from Temp import temp
 from Motion import motion
-from BUZZER import BUZZER_CL
+
