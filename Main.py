65ac2b4398152ced7fd78d2298da763d6e4e67eb
GHayden <ghayden@salisbury6c.ac.uk>
2016-04-20 11:40:26 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from BUTTON import BUTTON_CL
 from LED import LED_CL
 from CLOCK import CLOCK_CL
 from BUZZER import BUZZER_CL
-from Temp import temp
-from Motion import motion
+from TEMP import TEMP_CL
+from MOTION import MOTION_CL
+from PIN import PIN_CL
 
+def 
