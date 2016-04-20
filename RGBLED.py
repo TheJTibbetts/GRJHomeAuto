e4dda23e1827f399d6d539e7011e0243905813d4
Jake <jtibbetts@salisbury6c.ac.uk>
2016-04-20 11:48:57 +0100

    Update RGBLED.py
    May have finished this section

--- RGBLED.py
+++ RGBLED.py
@@ -1,3 +1,27 @@
 from LED import LED_CL
 from BOARD import BOARD_CL
-from BUTTON import BUTTON_CL
+
+class RGBLED_CL:
+  
+  def __init__(self, BOARD_CL, pin, LED_CL):
+    self.turnon = ledOn
+    self.turnoff = ledOff
+    self.board = BOARD_CL
+  
+  def RLEDon(self):
+    self.turnon
+  
+  def GLEDon(self):
+    self.turnon
+  
+  def BLEDon(self):
+    self.turnon
+
+  def RLEDoff(self):
+    self.turnoff
+  
+  def GLEDoff(self):
+    self.turnoff
+  
+  def BLEDoff(self):
+    self.turnoff
