38b5ace16941fba60d82439986a573a7e17b6f4c
Jake <jtibbetts@salisbury6c.ac.uk>
2016-04-22 09:30:56 +0100

    Update RGBLED.py

--- RGBLED.py
+++ RGBLED.py
@@ -3,10 +3,10 @@ from BOARD import BOARD_CL
 
 class RGBLED_CL:
   
-  def __init__(self, BOARD_CL, LED_CL):
+  def __init__(self, BOARD_CL, pin, LED_CL):
     self.turnon = LED_CL.ledOn
     self.turnoff = LED_CL.ledOff
-#    self.pin = pin
+    self.pin = pin
     self.board = BOARD_CL
   
   def RLEDon(self):
