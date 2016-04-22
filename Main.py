bdc52ce9bfd21c41ff70b4592431e901bf6149e8
GHayden <ghayden@salisbury6c.ac.uk>
2016-04-22 09:25:32 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,3 +7,43 @@ from BUZZER import BUZZER_CL
 from TEMP import TEMP_CL
 from MOTION import MOTION_CL
 from PIN import PIN_CL
+
+def clock():
+  try:
+    while True:
+      lcd.lcd_string("-CLOCK-", lcd.LCD_LINE_1)
+      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
+      time.sleep(0.1)
+    
+def temp():
+  try:
+    while True:
+      temp = int(read_temp())
+      read_string = str(temp)
+      print(read_temp())
+      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
+      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
+      if int(temp) >= 24:
+        GPIO.output(20, 0)
+        GPIO.output(16, 1)
+        #GPIO.output(24, 0)
+      elif int(temp) < 23.9:
+        GPIO.output(20, 1)
+        # GPIO.output(23, 0)
+        GPIO.output(16, 0)
+      time.sleep(0.1)
+
+def pirsensor():
+  try:
+    while True:
+      print 'debug:'+str(GPIO.input(PIR_PIN))
+      if GPIO.input(PIR_PIN) == True:
+        print 'debug: MOTION DETECTED'
+        GPIO.output(5,1)
+        time.sleep(1)
+        GPIO.output(5,0)
+        time.sleep(1)
+      elif GPIO.input(PIR_PIN) == False:
+        print 'debug: no motion detected'
+
+
