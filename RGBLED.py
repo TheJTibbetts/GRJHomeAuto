a0c1cd36cef11b1bf1f0bca565dbaec6cbf91c06
Jake <jtibbetts@salisbury6c.ac.uk>
2016-04-22 09:40:47 +0100

    Update RGBLED.py

--- RGBLED.py
+++ RGBLED.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
     from BOARD import BOARD_CL
     from time import sleep
     rpi = BOARD_CL()
-    rled = LED_CL(rpi, 16)
-    gled = LED_CL(rpi, 20)
-    bled = LED_CL(rpi, 21)
+    rled = RGBLED_CL(rpi, 16)
+    gled = RGBLED_CL(rpi, 20)
+    bled = RGBLED_CL(rpi, 21)
     rled.RLEDon()
     sleep(3)
     rled.RLEDoff()
