1d85b508e0d8ddd81d81f67a2795900d7df1179b
Robert James <rjames@salisbury6c.ac.uk>
2016-04-25 11:33:52 +0100

    Update MOTION.py

--- MOTION.py
+++ MOTION.py
@@ -1,7 +1,9 @@
 from BOARD import BOARD_CL
+from BUZZER import BUZZER_CL
 
 class Motion_CL:
   def __init__(self, BOARD_CL):
     self.board = BOARD_CL
+    self.buzzer = BUZZER_CL
     self.pin = pin
     self.setup_PIR
