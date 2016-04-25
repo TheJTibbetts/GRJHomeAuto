cf187309dd7cbafb69dfe8848ceaab737e641c55
GHayden <ghayden@salisbury6c.ac.uk>
2016-04-25 12:42:09 +0100

    Update MOTION.py

--- MOTION.py
+++ MOTION.py
@@ -2,7 +2,7 @@ from BOARD import BOARD_CL
 from BUZZER import BUZZER_CL
 
 class Motion_CL:
-  def __init__(self, BOARD_CL):
+  def __init__(self, BOARD_CL, BUZZER_CL):
     self.board = BOARD_CL
     self.buzzer = BUZZER_CL
     self.pin = pin
