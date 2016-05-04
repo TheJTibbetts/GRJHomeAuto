c80f3a835c634908c238e27ca1b478165ffe4688
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-04 12:01:29 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,16 @@ from BUZZER import BUZZER_CL
 from TEMP import TEMP_CL
 from MOTION import MOTION_CL
 
+os.system('modprobe w1-gpio')
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
 #ButtonPresses = [1,2,3,4]
+
+
+
 
 def clock():
   try:
