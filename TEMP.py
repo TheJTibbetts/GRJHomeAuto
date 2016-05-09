d6084fc83ba36785c37a3866a83adb756133d2d7
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-09 12:45:20 +0100

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -15,7 +15,7 @@ class TEMP_CL:
     def __init__(self, BOARD_CL):
         self.board = BOARD_CL
 #        self.setup_thermometer()
-        self.read_temp_raw()
+        self.read_temp_raw(device_file)
         self.read_temp()
     
     def read_temp_raw(self, device_file):
