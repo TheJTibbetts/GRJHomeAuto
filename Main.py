8211aab7c61b82ebe2e400d0f410929f5f4f3517
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-09 11:31:32 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,6 +16,7 @@ lcd=LCD1602_CL()
 tcl=TEMP_CL(4)
 
 os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
