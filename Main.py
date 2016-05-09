277a459b73a8b37e98490917a93795d692f938c8
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-09 12:51:14 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -34,7 +34,7 @@ device_file = device_folder + '/w1_slave'
 def temp(device_file):
   try:
     while True:
-      tcl.read_temp_raw()
+      tcl.read_temp_raw(device_file)
       rtemp = int(tcl.read_temp())
       read_string = str(rtemp)
       print(tcl.read_temp())
