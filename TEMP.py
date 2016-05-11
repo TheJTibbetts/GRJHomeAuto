428ee79959562a8c27dad84d9b58c4c7c49a54a1
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-11 11:19:43 +0100

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -17,7 +17,7 @@ class TEMP_CL:
 #        self.setup_thermometer()
         self.read_temp_raw()
         self.read_temp()
-        self.device_file()
+        self.device_file = device_file
     
     def read_temp_raw(self):
         f = open(device_file, 'r')
