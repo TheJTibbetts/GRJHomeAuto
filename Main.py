4696872bfbb2c3bbbd5e53dc699eda08f080f3d4
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-11 11:27:09 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -32,7 +32,7 @@ device_file = device_folder + '/w1_slave'
 #      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
 #      time.sleep(0.1)
 
-def temp(device_file):
+def temp():
   try:
     while True:
       tcl.read_temp_raw()
