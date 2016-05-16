1fba7913899eb415a1bc53924219e4703e054e4b
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-16 11:48:28 +0100

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -11,8 +11,9 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 global read_temp_raw
-read_temp_raw = str(read_temp_raw)
+str(read_temp_raw)
 print read_temp_raw
+
 class TEMP_CL:
     def __init__(self, BOARD_CL, read_temp_raw):
         self.board = BOARD_CL
