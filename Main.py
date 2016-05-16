f53ad35e1c2f5036209f8549778c3a65655cda0b
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-16 11:11:48 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,22 +33,22 @@ device_file = device_folder + '/w1_slave'
 #      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
 #      time.sleep(0.1)
 
-##def temp():
-#  try:
-#    while True:
-#      tcl.read_temp_raw()
-#      rtemp = int(tcl.read_temp())
-#      read_string = str(rtemp)
-#      print(tcl.read_temp())
-#      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
-#      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
-#      if int(rtemp) >= 24:
-#        GPIO.output(20, 0)
-#        GPIO.output(16, 1)
-#      elif int(rtemp) < 23.9:
-#        GPIO.output(20, 1)
-#        GPIO.output(16, 0)
-#      time.sleep(0.1)
+def temp():
+  try:
+    while True:
+      tcl.read_temp_raw()
+      rtemp = int(tcl.read_temp())
+      read_string = str(rtemp)
+      print(tcl.read_temp())
+      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
+      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
+      if int(rtemp) >= 24:
+        GPIO.output(20, 0)
+        GPIO.output(16, 1)
+      elif int(rtemp) < 23.9:
+        GPIO.output(20, 1)
+        GPIO.output(16, 0)
+      time.sleep(0.1)
 
 ##def piCamera():
 #   try:
@@ -56,17 +56,17 @@ device_file = device_folder + '/w1_slave'
 #       if GPIO.input(PIR_PIN) == True:
 #         pi_Cam
           
-def pirsensor():
-  try:
-    while True:
-      print 'debug:' +str (GPIO.input(PIR_PIN))
-      if GPIO.input(PIR_PIN) == True:
-        print 'debug: MOTION DETECTED'
-        lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
-        GPIO.output(5,1)
-        time.sleep(1)
-        GPIO.output(5,0)
-        time.sleep(1)
+#def pirsensor():
+#  try:
+#    while True:
+#      print 'debug:' +str (GPIO.input(PIR_PIN))
+#      if GPIO.input(PIR_PIN) == True:
+#        print 'debug: MOTION DETECTED'
+#        lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
+#        GPIO.output(5,1)
+#        time.sleep(1)
+#        GPIO.output(5,0)
+#        time.sleep(1)
 
 
   except KeyboardInterrupt:
