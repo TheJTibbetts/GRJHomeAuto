2f5b9299a514dc6be78089ee85dbff83ccfea817
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-16 11:21:54 +0100

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -10,7 +10,7 @@ os.system('modprobe w1-gpio')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-read_temp_raw = global
+global read_temp_raw
 read_temp_raw = 0
 
 class TEMP_CL:
