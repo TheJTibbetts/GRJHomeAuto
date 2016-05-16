1f223fefa38d4b8717242a3c71e6b870c2123392
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-16 12:56:20 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,19 +1,40 @@
 from BOARD import BOARD_CL
 
 class LED_CL:
-  def __init__(self, BOARD_CL, pin):
+  def __init__(self, BOARD_CL, 6, 16, 20):
     self.board = BOARD_CL
-    self.pin = pin
+    self.pinW = 6
+    self.pinR = 16
+    self.pinB = 20
     self.setup_led()
   
   def setup_led(self):
-    self.board.GPIO.setup(self.pin, self.board.GPIO.OUT)
+    self.board.GPIO.setup(self.pinW, self.board.GPIO.OUT)
   
   def ledOn(self):
-    self.board.GPIO.output(self.pin, self.board.GPIO.HIGH)
+    self.board.GPIO.output(self.pinW, self.board.GPIO.HIGH)
   
   def ledOff(self):
-    self.board.GPIO.output(self.pin, self.board.GPIO.LOW)
+    self.board.GPIO.output(self.pinW, self.board.GPIO.LOW)
+    
+  def setup_led(self):
+    self.board.GPIO.setup(self.pinR, self.board.GPIO.OUT)
+  
+  def ledOn(self):
+    self.board.GPIO.output(self.pinR, self.board.GPIO.HIGH)
+  
+  def ledOff(self):
+    self.board.GPIO.output(self.pinR, self.board.GPIO.LOW)
+
+  def setup_led(self):
+    self.board.GPIO.setup(self.pinB, self.board.GPIO.OUT)
+  
+  def ledOn(self):
+    self.board.GPIO.output(self.pinB, self.board.GPIO.HIGH)
+  
+  def ledOff(self):
+    self.board.GPIO.output(self.pinB, self.board.GPIO.LOW)
+
 
 if __name__ == "__main__":
     from BOARD import BOARD_CL
