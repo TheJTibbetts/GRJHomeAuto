068a960001adb33374d264595073fda620759380
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-16 11:22:44 +0100

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -11,7 +11,7 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 global read_temp_raw
-read_temp_raw = 0
+#read_temp_raw = 0
 
 class TEMP_CL:
     def __init__(self, BOARD_CL):
