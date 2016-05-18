42a1b47918d0233c4e8a8b3f4e12bda177c57d50
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-18 11:28:07 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ import glob
 
 lcd=LCD1602_CL()
 tcl=TEMP_CL(4)
-#led=LED_CL(6, 16, 20)
+led=LED_CL(6, 16, 20)
 
 os.system('modprobe w1-gpio')
 base_dir = '/sys/bus/w1/devices/'
@@ -37,8 +37,10 @@ device_file = device_folder + '/w1_slave'
 def temp():
   try:
     while True:
-      GPIO.setup(20, GPIO.OUT)
-      GPIO.setup(16, GPIO.OUT)
+      led.setup_ledR(16)
+      led.setup_ledB(20)
+      #GPIO.setup(20, GPIO.OUT)
+      #GPIO.setup(16, GPIO.OUT)
       rtemp = int(tcl.read_temp())
       read_string = str(rtemp)
       print(tcl.read_temp())
