4ad0fd5eb0492993f65ab7f264d613397e91629b
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-23 11:47:20 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,7 +1,7 @@
 from BOARD import BOARD_CL
  
 class LED_CL:
-  def __init__(self, BOARD_CL, 6):
+  def __init__(self, BOARD_CL, pinW, pinR, PinB):
     self.board = BOARD_CL
     self.pinW = 6
     self.pinR = 16
