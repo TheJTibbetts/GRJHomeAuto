1a8b6fda46d98377975e60274a66e0f380446e0f
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-23 12:48:56 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,23 +5,42 @@ import time
 from LCD import LCD1602_CL
 from BUTTON import BUTTON_CL
 
-while True:
-    
-    count = 0
-    btnPress = count + 1
-    
-    if btnPress == 1:
-        run HowTo.py
+a = ['one', 'two', 'three', 'four', 'five']
+count = 0
 
-    if btnPress == 2:
-        run Clock.py
-    
-    if btnPress == 3:
-        run Temp.py
-    
-    if btnPress == 4:
-        run Motion.py
-    
-    if btnPress == 5:
-        run Light.py
+def button.pressed():
+# Run the first program
+    count += 1
+# Run the second program
+    count += 1
+# Run the third program
+    count += 1
+# Run the fourth program
+    count += 1
+# Run the fifth program
+    if count == 4:
         count = 0
+
+
+
+#while True:
+#    
+#    count = 0
+#    btnPress = count + 1
+#    
+#    if btnPress == 1:
+#        run HowTo.py
+#
+#    if btnPress == 2:
+#        run Clock.py
+#    
+#    if btnPress == 3:
+#        run Temp.py
+#    
+#    if btnPress == 4:
+#        run Motion.py
+#    
+#    if btnPress == 5:
+#        run Light.py
+#        count = 0
+#
