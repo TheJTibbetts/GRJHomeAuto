54cc9a1d866cdd946e014441a1246b5fff39b15c
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-23 11:35:25 +0100

    Update Pirmain.py

--- Pirmain.py
+++ Pirmain.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from BOARD import BOARD_CL
 from LCD import LCD1602_CL
 from BUTTON import BUTTON_CL
-from LED import LED_CL
+#from LED import LED_CL
 from BUZZER import BUZZER_CL
 from TEMP import TEMP_CL
 from MOTION import MOTION_CL
