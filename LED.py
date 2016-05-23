f293a72e3821a25b339ad8b8291c3843944c8eba
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-23 12:37:53 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -10,7 +10,7 @@ class LED_CL:
     self.pinB = 20
     self.setup_led()
    
-  def setup_led(self, pinW, pinB, PinR):
+  def setup_led(self, pinW, pinB, pinR):
     GPIO.setup(self.pinW, GPIO.OUT)
     GPIO.setup(self.pinB, GPIO.OUT)
     GPIO.setup(self.pinR, GPIO.OUT)
