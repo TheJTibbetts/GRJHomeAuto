a7a9ca440f0c902576722b1e2a0b8b3fdac92107
Robert James <rjames@salisbury6c.ac.uk>
2016-05-24 10:23:18 +0100

    Update howTo.py

--- howTo.py
+++ howTo.py
@@ -3,15 +3,27 @@ from time import sleep
 
 lcd = LCD1602()
 
-lcd.lcd_string("Please hold for",lcd.LCD_LINE_1)
-lcd.lcd_string("help...",lcd.LCD_LINE_2)
+lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
+lcd.lcd_string("for help",lcd.LCD_LINE_2)
 
 time.sleep(5)
 
-lcd.lcd_string("To continue to",lcd.LCD_LINE_1)
-lcd.lcd_string("the next ",lcd.LCD_LINE_2)
+lcd.lcd_string("Button 1:",lcd.LCD_LINE_1)
+lcd.lcd_string("Decrease values",lcd.LCD_LINE_2)
 
+time.sleep(5)
 
+lcd.lcd_string("Button 2:",lcd.LCD_LINE_1)
+lcd.lcd_string("Increase value",lcd.LCD_LINE_2)
 
-lcd.lcd_string("To start press",lcd.LCD_LINE_1)
-lcd.lcd_string("button 3",lcd.LCD_LINE_2)
+time.sleep(5)
+
+lcd.lcd_string("Button 3:",lcd.LCD_LINE_1)
+lcd.lcd_string("Cycle programs",lcd.LCD_LINE_2)
+
+time.sleep(5)
+
+lcd.lcd_string("Button 4:",lcd.LCD_LINE_1)
+lcd.lcd_string("Ajust LED",lcd.LCD_LINE_2)
+
+lcd.cleanup()
