a08411685f0ccab3c10ba9d39d3e9f671cc10288
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-25 11:39:29 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,42 +5,44 @@ import time
 from LCD import LCD1602_CL
 from BUTTON import BUTTON_CL
 
+lcd = LCD1602_CL()
+
 a = ['one', 'two', 'three', 'four', 'five']
 count = 0
 
 def button.pressed():
-# Run the first program
+
+lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
+lcd.lcd_string("for help",lcd.LCD_LINE_2)
+
+time.sleep(3)
+
+lcd.lcd_string("Button 1:",lcd.LCD_LINE_1)
+lcd.lcd_string("Decrease Temp C",lcd.LCD_LINE_2)
+
+time.sleep(3)
+
+lcd.lcd_string("Button 2:",lcd.LCD_LINE_1)
+lcd.lcd_string("Increase Temp C",lcd.LCD_LINE_2)
+
+time.sleep(3)
+
+lcd.lcd_string("Button 3:",lcd.LCD_LINE_1)
+lcd.lcd_string("Cycle programs",lcd.LCD_LINE_2)
+
+time.sleep(3)
+
+lcd.lcd_string("Button 4:",lcd.LCD_LINE_1)
+lcd.lcd_string("Adjust LED",lcd.LCD_LINE_2)
+
+time.sleep(3)
     count += 1
-# Run the second program
+clock()
     count += 1
-# Run the third program
+temp()
     count += 1
-# Run the fourth program
+pirsensor()
     count += 1
-# Run the fifth program
+LED()
     if count == 4:
         count = 0
-
-
-
-#while True:
-#    
-#    count = 0
-#    btnPress = count + 1
-#    
-#    if btnPress == 1:
-#        run HowTo.py
-#
-#    if btnPress == 2:
-#        run Clock.py
-#    
-#    if btnPress == 3:
-#        run Temp.py
-#    
-#    if btnPress == 4:
-#        run Motion.py
-#    
-#    if btnPress == 5:
-#        run Light.py
-#        count = 0
-#
