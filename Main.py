87a5870f2da8351680b0eb3cfa9c40546837a3e9
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-25 12:38:36 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,70 +23,121 @@ os.system('modprobe w1-gpio')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-#ButtonPresses = [1,2,3,4]
 
 
 
+def clock():
+  try:
+    while True:
+      lcd.lcd_string("Clock in GMT", lcd.LCD_LINE_1)
+      lcd.lcd_string("now running", lcd.LCD_LINE_2)
+      time.sleep(3)
+      lcd.lcd_string("-CLOCK-", lcd.LCD_LINE_1)
+      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
+      time.sleep(0.1)
 
-#def clock():
-#  try:
-#    while True:
-#      lcd.lcd_string("-CLOCK-", lcd.LCD_LINE_1)
-#      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
-#      time.sleep(0.1)
+def temp():
+  try:
+    while True:
+      lcd.lcd_string("Temperature", lcd.LCD_LINE_1)
+      lcd.lcd_string("now running", lcd.LCD_LINE_2)
+      time.sleep(3)
+      led.setup_led()
+      #GPIO.setup(20, GPIO.OUT)
+      #GPIO.setup(16, GPIO.OUT)
+      rtemp = int(tcl.read_temp())
+      read_string = str(rtemp)
+      print(tcl.read_temp())
+      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
+      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
+      if int(rtemp) >= 24:
+        #GPIO.output(16, 1)
+        led.ledOnR()
+        led.ledOffB()
+        #print(read_string)
+      elif int(rtemp) < 23.9:
+        #GPIO.output(20, 1)
+        led.ledOnB()
+        led.ledOffR()
+      
+        #print(read_string)
+      time.sleep(0.1)
 
-#def temp():
-#  try:
-#    while True:
-#      led.setup_led()
-#      #GPIO.setup(20, GPIO.OUT)
-#      #GPIO.setup(16, GPIO.OUT)
-#      rtemp = int(tcl.read_temp())
-#      read_string = str(rtemp)
-#      print(tcl.read_temp())
-#      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
-#      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
-#      if int(rtemp) >= 24:
-#        #GPIO.output(16, 1)
-#        led.ledOnR()
-#        led.ledOffB()
-#        #print(read_string)
-#      elif int(rtemp) < 23.9:
-#        #GPIO.output(20, 1)
-#        led.ledOnB()
-#        led.ledOffR()
-#      
-#        #print(read_string)
-#      time.sleep(0.1)
-
-#def pirsensor():
-#  try:
-#    while True:
-#      buzz.setup_buzzer()
-#      mot.setup_pir()
-#      #GPIO.setup(22, GPIO.IN)
-#      #GPIO.setup(5, GPIO.OUT)
-#      print('setup')
-#      if GPIO.input(22) == True:
-#        print ('debug: MOTION DETECTED')
-#        lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
-#        #os.system('python pi_Cam.py')
-#        buzz.buzzOn()
-#        #GPIO.output(5,1)
-#        time.sleep(1)
-#        buzz.buzzOff()
-#        #GPIO.output(5,0)
-#        time.sleep(1)
-#      elif GPIO.input(22) == False:
-#        lcd.lcd_string("", lcd.LCD_LINE_2)
+def pirsensor():
+  try:
+    while True:
+      lcd.lcd_string("Motion detector", lcd.LCD_LINE_1)
+      lcd.lcd_string("now running", lcd.LCD_LINE_2)
+      time.sleep(3)
+      buzz.setup_buzzer()
+      mot.setup_pir()
+      #GPIO.setup(22, GPIO.IN)
+      #GPIO.setup(5, GPIO.OUT)
+      print('setup')
+      if GPIO.input(22) == True:
+        print ('debug: MOTION DETECTED')
+        lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
+        #os.system('python pi_Cam.py')
+        buzz.buzzOn()
+        #GPIO.output(5,1)
+        time.sleep(1)
+        buzz.buzzOff()
+        #GPIO.output(5,0)
+        time.sleep(1)
+      elif GPIO.input(22) == False:
+        lcd.lcd_string("", lcd.LCD_LINE_2)
 
 def LED():
   try:
     while True:
+      lcd.lcd_string("LED dimmer", lcd.LCD_LINE_1)
+      lcd.lcd_string("now running", lcd.LCD_LINE_2)
+      time.sleep(3)
       led.ledOnW()
 
   except KeyboardInterrupt:
     lcd.cleanup()
     GPIO.cleanup()
 
+def howto:
+    lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
+    lcd.lcd_string("for help",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 1:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Decrease Temp C",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 2:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Increase Temp C",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 3:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Cycle programs",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 4:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Adjust LED",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+a = ['one', 'two', 'three', 'four', 'five']
+count = 0
+
+def button.pressed():
+howto()
+    count += 1
+clock()
+    count += 1
+temp()
+    count += 1
+pirsensor()
+    count += 1
 LED()
+    if count == 4:
+        count = 0
+w
