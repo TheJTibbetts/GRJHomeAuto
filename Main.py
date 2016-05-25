dd93a5c058cf233cd698d25bffc5c1101b058dd6
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-25 12:45:45 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,81 +24,72 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-
+try:
 
 def clock():
-  try:
-    while True:
-      lcd.lcd_string("Clock in GMT", lcd.LCD_LINE_1)
-      lcd.lcd_string("now running", lcd.LCD_LINE_2)
-      time.sleep(3)
-      lcd.lcd_string("-CLOCK-", lcd.LCD_LINE_1)
-      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
-      time.sleep(0.1)
-  except:
+  while True:
+    lcd.lcd_string("Clock in GMT", lcd.LCD_LINE_1)
+    lcd.lcd_string("now running", lcd.LCD_LINE_2)
+    time.sleep(3)
+    lcd.lcd_string("-CLOCK-", lcd.LCD_LINE_1)
+    lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
+    time.sleep(0.1)
+
 
 def temp():
-  try:
-    while True:
-      lcd.lcd_string("Temperature", lcd.LCD_LINE_1)
-      lcd.lcd_string("now running", lcd.LCD_LINE_2)
-      time.sleep(3)
-      led.setup_led()
-      #GPIO.setup(20, GPIO.OUT)
-      #GPIO.setup(16, GPIO.OUT)
-      rtemp = int(tcl.read_temp())
-      read_string = str(rtemp)
-      print(tcl.read_temp())
-      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
-      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
-      if int(rtemp) >= 24:
-        #GPIO.output(16, 1)
-        led.ledOnR()
-        led.ledOffB()
-        #print(read_string)
-      elif int(rtemp) < 23.9:
-        #GPIO.output(20, 1)
-        led.ledOnB()
-        led.ledOffR()
-      
-        #print(read_string)
-      time.sleep(0.1)
+  while True:
+    lcd.lcd_string("Temperature", lcd.LCD_LINE_1)
+    lcd.lcd_string("now running", lcd.LCD_LINE_2)
+    time.sleep(3)
+    led.setup_led()
+    #GPIO.setup(20, GPIO.OUT)
+    #GPIO.setup(16, GPIO.OUT)
+    rtemp = int(tcl.read_temp())
+    read_string = str(rtemp)
+    print(tcl.read_temp())
+    lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
+    lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
+    if int(rtemp) >= 24:
+      #GPIO.output(16, 1)
+      led.ledOnR()
+      led.ledOffB()
+      #print(read_string)
+    elif int(rtemp) < 23.9:
+      #GPIO.output(20, 1)
+      led.ledOnB()
+      led.ledOffR()
+      #print(read_string)
+    time.sleep(0.1)
 
 def pirsensor():
-  try:
-    while True:
-      lcd.lcd_string("Motion detector", lcd.LCD_LINE_1)
-      lcd.lcd_string("now running", lcd.LCD_LINE_2)
-      time.sleep(3)
-      buzz.setup_buzzer()
-      mot.setup_pir()
-      #GPIO.setup(22, GPIO.IN)
-      #GPIO.setup(5, GPIO.OUT)
-      print('setup')
-      if GPIO.input(22) == True:
-        print ('debug: MOTION DETECTED')
-        lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
-        #os.system('python pi_Cam.py')
-        buzz.buzzOn()
-        #GPIO.output(5,1)
-        time.sleep(1)
-        buzz.buzzOff()
-        #GPIO.output(5,0)
-        time.sleep(1)
-      elif GPIO.input(22) == False:
-        lcd.lcd_string("", lcd.LCD_LINE_2)
+  while True:
+    lcd.lcd_string("Motion detector", lcd.LCD_LINE_1)
+    lcd.lcd_string("now running", lcd.LCD_LINE_2)
+    time.sleep(3)
+    buzz.setup_buzzer()
+    mot.setup_pir()
+    #GPIO.setup(22, GPIO.IN)
+    #GPIO.setup(5, GPIO.OUT)
+    print('setup')
+    if GPIO.input(22) == True:
+      print ('debug: MOTION DETECTED')
+      lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
+      #os.system('python pi_Cam.py')
+      buzz.buzzOn()
+      #GPIO.output(5,1)
+      time.sleep(1)
+      buzz.buzzOff()
+      #GPIO.output(5,0)
+      time.sleep(1)
+    elif GPIO.input(22) == False:
+      lcd.lcd_string("", lcd.LCD_LINE_2)
 
 def LED():
-  try:
-    while True:
-      lcd.lcd_string("LED dimmer", lcd.LCD_LINE_1)
-      lcd.lcd_string("now running", lcd.LCD_LINE_2)
-      time.sleep(3)
-      led.ledOnW()
-
-  except KeyboardInterrupt:
-    lcd.cleanup()
-    GPIO.cleanup()
+  while True:
+    lcd.lcd_string("LED dimmer", lcd.LCD_LINE_1)
+    lcd.lcd_string("now running", lcd.LCD_LINE_2)
+    time.sleep(3)
+    led.ledOnW()
 
 def howto:
     lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
@@ -141,4 +132,7 @@ pirsensor()
 LED()
     if count == 4:
         count = 0
-w
+
+except KeyboardInterrupt:
+  lcd.cleanup()
+  GPIO.cleanup()
