694727c61cd5a0a692d2413821b440c4349fbc48
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-25 11:46:13 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,35 +7,37 @@ from BUTTON import BUTTON_CL
 
 lcd = LCD1602_CL()
 
+def howto:
+    lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
+    lcd.lcd_string("for help",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 1:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Decrease Temp C",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 2:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Increase Temp C",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 3:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Cycle programs",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
+    lcd.lcd_string("Button 4:",lcd.LCD_LINE_1)
+    lcd.lcd_string("Adjust LED",lcd.LCD_LINE_2)
+
+    time.sleep(3)
+
 a = ['one', 'two', 'three', 'four', 'five']
 count = 0
 
 def button.pressed():
-
-lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
-lcd.lcd_string("for help",lcd.LCD_LINE_2)
-
-time.sleep(3)
-
-lcd.lcd_string("Button 1:",lcd.LCD_LINE_1)
-lcd.lcd_string("Decrease Temp C",lcd.LCD_LINE_2)
-
-time.sleep(3)
-
-lcd.lcd_string("Button 2:",lcd.LCD_LINE_1)
-lcd.lcd_string("Increase Temp C",lcd.LCD_LINE_2)
-
-time.sleep(3)
-
-lcd.lcd_string("Button 3:",lcd.LCD_LINE_1)
-lcd.lcd_string("Cycle programs",lcd.LCD_LINE_2)
-
-time.sleep(3)
-
-lcd.lcd_string("Button 4:",lcd.LCD_LINE_1)
-lcd.lcd_string("Adjust LED",lcd.LCD_LINE_2)
-
-time.sleep(3)
+howto()
     count += 1
 clock()
     count += 1
