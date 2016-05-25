a3f195e05cf4562914def744518a48f81ae9e6df
Jake <jtibbetts@salisbury6c.ac.uk>
2016-05-25 11:52:52 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -34,53 +34,55 @@ device_file = device_folder + '/w1_slave'
 #      lcd.lcd_string(time.strftime("%H" + ":" + "%M" + ":" + "%S"), lcd.LCD_LINE_2)
 #      time.sleep(0.1)
 
-def temp():
-  try:
-    while True:
-      led.setup_led()
-      #GPIO.setup(20, GPIO.OUT)
-      #GPIO.setup(16, GPIO.OUT)
-      rtemp = int(tcl.read_temp())
-      read_string = str(rtemp)
-      print(tcl.read_temp())
-      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
-      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
-      if int(rtemp) >= 24:
-        #GPIO.output(16, 1)
-        led.ledOnR()
-        led.ledOffB()
-        #print(read_string)
-      elif int(rtemp) < 23.9:
-        #GPIO.output(20, 1)
-        led.ledOnB()
-        led.ledOffR()
-      
-        #print(read_string)
-      time.sleep(0.1)
-
+#def temp():
+#  try:
+#    while True:
+#      led.setup_led()
+#      #GPIO.setup(20, GPIO.OUT)
+#      #GPIO.setup(16, GPIO.OUT)
+#      rtemp = int(tcl.read_temp())
+#      read_string = str(rtemp)
+#      print(tcl.read_temp())
+#      lcd.lcd_string("-TEMPERATURE-", lcd.LCD_LINE_1)
+#      lcd.lcd_string(read_string + " C", lcd.LCD_LINE_2)
+#      if int(rtemp) >= 24:
+#        #GPIO.output(16, 1)
+#        led.ledOnR()
+#        led.ledOffB()
+#        #print(read_string)
+#      elif int(rtemp) < 23.9:
+#        #GPIO.output(20, 1)
+#        led.ledOnB()
+#        led.ledOffR()
+#      
+#        #print(read_string)
+#      time.sleep(0.1)
 
 #def pirsensor():
 #  try:
 #    while True:
-#      GPIO.setup(32, GPIO.IN)
-#        GPIO.setup(5, GPIO.OUT)
-#        if GPIO.input(32) == True:
-#          print ('debug: MOTION DETECTED')
-#            lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
-#            os.system('python pi_Cam.py')
-#            GPIO.output(5,1)
-#            time.sleep(1)
-#            GPIO.output(5,0)
-#            time.sleep(1)
-#        elif GPIO.input(32) == False:
-#            lcd.lcd_string("", lcd.LCD_LINE_2)
+#      buzz.setup_buzzer()
+#      mot.setup_pir()
+#      #GPIO.setup(22, GPIO.IN)
+#      #GPIO.setup(5, GPIO.OUT)
+#      print('setup')
+#      if GPIO.input(22) == True:
+#        print ('debug: MOTION DETECTED')
+#        lcd.lcd_string("MOTION DETECTED", lcd.LCD_LINE_2)
+#        #os.system('python pi_Cam.py')
+#        buzz.buzzOn()
+#        #GPIO.output(5,1)
+#        time.sleep(1)
+#        buzz.buzzOff()
+#        #GPIO.output(5,0)
+#        time.sleep(1)
+#      elif GPIO.input(22) == False:
+#        lcd.lcd_string("", lcd.LCD_LINE_2)
 
-
-
-##def LED():
-#   try:
-#     while True:
-#       
+def LED():
+  try:
+    while True:
+      
 
   except KeyboardInterrupt:
     lcd.cleanup()
