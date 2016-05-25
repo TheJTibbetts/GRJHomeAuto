2db63b85dc30f47d50c7773f2f708a19b90713f4
Robert James <rjames@salisbury6c.ac.uk>
2016-05-25 08:51:46 +0100

    Update howTo.py

--- howTo.py
+++ howTo.py
@@ -6,24 +6,26 @@ lcd = LCD1602()
 lcd.lcd_string("Please read this",lcd.LCD_LINE_1)
 lcd.lcd_string("for help",lcd.LCD_LINE_2)
 
-time.sleep(5)
+time.sleep(3)
 
 lcd.lcd_string("Button 1:",lcd.LCD_LINE_1)
 lcd.lcd_string("Decrease values",lcd.LCD_LINE_2)
 
-time.sleep(5)
+time.sleep(3)
 
 lcd.lcd_string("Button 2:",lcd.LCD_LINE_1)
 lcd.lcd_string("Increase value",lcd.LCD_LINE_2)
 
-time.sleep(5)
+time.sleep(3)
 
 lcd.lcd_string("Button 3:",lcd.LCD_LINE_1)
 lcd.lcd_string("Cycle programs",lcd.LCD_LINE_2)
 
-time.sleep(5)
+time.sleep(3)
 
 lcd.lcd_string("Button 4:",lcd.LCD_LINE_1)
 lcd.lcd_string("Ajust LED",lcd.LCD_LINE_2)
+
+time.sleep(3)
 
 lcd.cleanup()
