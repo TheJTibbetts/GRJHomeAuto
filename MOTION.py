c422135d1fdb27fc8d797f628ff26c6e12328793
GHayden <ghayden@salisbury6c.ac.uk>
2016-05-25 11:22:20 +0100

    Update MOTION.py

--- MOTION.py
+++ MOTION.py
@@ -3,29 +3,29 @@ import RPi.GPIO as GPIO
 
 class MOTION_CL:
     def __init__(self, pinP):
-        self.__pinP = 32
-        self.__setup_pir()
-        self.__triggered = False
+        self.pinP = 32
+        self.setup_pir()
+        self.triggered = False
 
     @property
     def triggered(self):
-        return self.__pressed
+        return self.pressed
 
     @triggered.setter
     def triggered(self, value):
-        self.__pressed = value
+        self.pressed = value
 
 
-    def __setup_pir(self, resistor=False):
+    def setup_pir(self, resistor=False):
         if not resistor:
-            GPIO.setup(self.__pinP, GPIO.IN)
+            GPIO.setup(self.pinP, GPIO.IN)
         elif resistor:
-            GPIO.set(self.__pinP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.set(self.pinP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         else:
             print('Pir error')
         
         # add interrupt
-        GPIO.add_event_detect(self.__pinP, GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
+        GPIO.add_event_detect(self.pinP, GPIO.FALLING, callback=self.my_callback, bouncetime=300)
         
-    def __my_callback(self, channel):
+    def my_callback(self, channel):
         self.pressed = True
